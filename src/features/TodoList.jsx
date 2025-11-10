56cb4fe3315807aeb0b52965568c9ef53c6805ab
vinayak <vt869309@gmail.com>
2025-11-10 18:05:39 +0530

    allah

--- src/features/TodoList.jsx
+++ src/features/TodoList.jsx
@@ -20,17 +20,34 @@ function onhandleDelete(id){
   setTodos((todos)=>todos.filter((todo)=>todo.id !==id))
 }
 
- function onhandleSubmit(e){
 
-   e.preventDefault();
+function onhandleSubmit(e){
+  
+  e.preventDefault();
   const clean = description.trim();
-   if(!clean.trim()) return;
-
-  const newItem = {id:Date.now(),description:clean}
+  if(!clean.trim()) return;
+  
+  const newItem = {id:Date.now(),description:clean,completed:false}
   onhandleAdd(newItem)
   setDescription("")
+}
+
+function onhandletoggleitems(id){
+  setTodos((todos)=>
+  todos.map((todo)=>
+  todo.id === id ? {...todo,completed:!todo.completed} : todo
+  ))
+}
+
+ function onhandleClearList(){
+   const confirmed = window.confirm("Are you sure you want to delete this...")
+
+   if(confirmed) setTodos([])
  }
 
+ const completedCount = todos.filter((t)=>t.completed).length;
+ const total = todos.length;
+ const percentage = total === 0 ? 0 : Math.round((completedCount / total) * 100);
 
   return (
     <>
@@ -51,14 +68,18 @@ function onhandleDelete(id){
     tracking-[-2px]
     [word-spacing:6px] 
     sm:[word-spacing:14px]
-    lg:[word-spacing:30px]">Offline TODO</h1> 
+    lg:[word-spacing:30px]"> Offline TODO</h1> 
+        
           <form onSubmit={onhandleSubmit}>
 
          <div className="flex items-center gap-3 w-full max-w-xl mx-auto mt-10 px-4">
             <input placeholder="Type what you want to do" value={description} onChange={(e)=>setDescription(e.target.value)}
             className="w-100 border border-gray-300 rounded-md px-2 py-2 focus:outline-none focus:ring-2 focus:ring-yellow-400"
             />
-              <button className="bg-yellow-400 text-white font-bold px-5 py-2 rounded-md hover:bg-yellow-500 transition cursor-pointer" onClick={onhandleAdd}>
+              <button
+              type="submit" 
+              className="bg-yellow-400 text-white font-bold px-5 py-2 rounded-md hover:bg-yellow-500 transition cursor-pointer" 
+              onClick={onhandleAdd}>
                 Add
               </button>
             </div>
@@ -67,14 +88,20 @@ function onhandleDelete(id){
             <ul className="max-w-xl py-10 mx-auto mt-20 font-bold flex flex-wrap gap-10 px-4">
              {todos.map((todo)=>
              <li key={todo.id}>
-              <span>{todo.description}</span>             
+              <input type="checkbox" 
+              checked={todo.completed}
+              onChange={()=>onhandletoggleitems(todo.id)}/>
+              <span className={todo.completed ? "line-through opacity-45": ""}>{todo.description}</span>             
               <button onClick={()=>onhandleDelete(todo.id)} className="cursor-pointer">❌</button>
              </li>
             )}
             </ul>
+
+            <h1 className="mt-40 mx-10 font-bold p-20 bg-amber-400 text-white">your success {percentage}% </h1>
         </div>
         </>
   )
-
 }
+
+
 export default TodoList
